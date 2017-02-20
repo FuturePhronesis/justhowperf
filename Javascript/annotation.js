60afc01cc6c554e232b2044417f260d664305396
Thomas Moore <tmoore712@gmail.com>
2017-02-20 17:48:36 -0500

    I've got the accordions working and now just need to work on getting each link styled differently.

--- Javascript/annotation.js
+++ Javascript/annotation.js
@@ -1,19 +1,9 @@
-/**
- * Created by thomasmoore on 2/17/17.
- */
-/**
- * Created by thomasmoore on 2/17/17.
- */
-var acc = document.getElementsByClassName("annotation");
+var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].onclick = function(){
-        /* Toggle between adding and removing the "active" class,
-         to highlight the button that controls the panel */
         this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
