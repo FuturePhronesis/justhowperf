8d94dd558b40e5196bbd4a959ee4a050de23b968
Thomas Moore <tmoore712@gmail.com>
2017-02-20 22:15:25 -0500

    Working on nav bar and home page

--- Javascript/testpage.js
+++ Javascript/testpage.js
@@ -53,3 +53,29 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+// ---BLUE---
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-blue .accordion-section-title-blue').removeClass('active');
+        $('.accordion-blue .accordion-section-content-blue').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-blue').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-blue ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
