a3ff3bcff65cb18cdf22fb3515a0a1b76552cf17
Thomas Moore <tmoore712@gmail.com>
2017-02-20 19:46:20 -0500

    I did it! Got the accordions working and styled differently.

--- Javascript/testpage.js
+++ Javascript/testpage.js
@@ -26,3 +26,30 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+// ---RED---
+
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-red .accordion-section-title-red').removeClass('active');
+        $('.accordion-red .accordion-section-content-red').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-red').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-red ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
