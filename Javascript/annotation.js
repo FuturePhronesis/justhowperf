ef4d07a395f4c65cdf22383a3455968ba9eddaf9
Thomas Moore <tmoore712@gmail.com>
2017-02-21 20:14:33 -0500

    Filling out the bannon speech

--- Javascript/annotation.js
+++ Javascript/annotation.js
@@ -1,14 +1,133 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+/**
+ * Created by thomasmoore on 2/20/17.
+ */
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion .accordion-section-title').removeClass('active');
+        $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
     }
-}
+
+    $('.accordion-section-title').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+
+// ---RED---
+
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-red .accordion-section-title-red').removeClass('active');
+        $('.accordion-red .accordion-section-content-red').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-red').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-red ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+
+// ---BLUE---
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-blue .accordion-section-title-blue').removeClass('active');
+        $('.accordion-blue .accordion-section-content-blue').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-blue').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-blue ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+
+// ----GREEN-----
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-green .accordion-section-title-green').removeClass('active');
+        $('.accordion-green .accordion-section-content-green').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-green').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-green ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+
+// -------TEAL-------
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-teal .accordion-section-title-teal').removeClass('active');
+        $('.accordion-teal .accordion-section-content-teal').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-teal').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-teal ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
--- Javascript/testpage.js
+++ Javascript/testpage.js
@@ -79,3 +79,55 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+// ----GREEN-----
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-green .accordion-section-title-green').removeClass('active');
+        $('.accordion-green .accordion-section-content-green').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-green').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-green ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
+
+// -------TEAL-------
+$(document).ready(function() {
+    function close_accordion_section() {
+        $('.accordion-teal .accordion-section-title-teal').removeClass('active');
+        $('.accordion-teal .accordion-section-content-teal').slideUp(300).removeClass('open');
+    }
+
+    $('.accordion-section-title-teal').click(function(e) {
+        // Grab current anchor value
+        var currentAttrValue = $(this).attr('href');
+
+        if($(e.target).is('.active')) {
+            close_accordion_section();
+        }else {
+            close_accordion_section();
+
+            // Add active class to section title
+            $(this).addClass('active');
+            // Open up the hidden content panel
+            $('.accordion-teal ' + currentAttrValue).slideDown(300).addClass('open');
+        }
+
+        e.preventDefault();
+    });
+});
